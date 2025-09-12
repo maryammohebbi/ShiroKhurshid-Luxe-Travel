f91057a3358d6cb501750c36c5be3e4b964559c2
Maryam Mohebbi <maryam.mohebbi.jr@gmail.com>
2025-09-12 19:05:42 +0330

    project started

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -4,9 +4,9 @@ import { RxHamburgerMenu } from "react-icons/rx"
 function Login() {
   return (
     <div>
-        <div className='flex justify-between h-[58px] p-2 font-bold'>
+        <div className='flex justify-between items-center h-[58px] p-5 font-bold'>
             <span className='text-[#BB9039]'>Shir O Khurshid</span>
-            <button className='text-[#BB9039] flex items-center justify-center'><RxHamburgerMenu/></button>
+            <button className='text-[#BB9039] flex items-center justify-center'><RxHamburgerMenu className='w-6 h-6'/></button>
         </div>
     </div>
   )
